4f53772434fd65e90aecbd2e16c66e7d2b0f5be3
Guilherme Fickel <guilhermefickel@gmail.com>
2018-10-29 16:37:13 -0200

    Code and windows clean up

--- example/main.js
+++ example/main.js
@@ -105,58 +105,16 @@ System.register(["imgui-js", "./imgui_impl", "imgui-js/imgui_demo", "imgui-js/im
         ImGui_Impl.NewFrame(time);
         ImGui.NewFrame();
 
-
-
-        // const Http = new XMLHttpRequest();
-        // const url='https://jsonplaceholder.typicode.com/posts';
-        // Http.open("GET", url);
-        // Http.send();
-        // Http.onreadystatechange=(e)=>{
-        //     console.log(Http.responseText)
-        // }
-
-
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (!done && show_demo_window) {
-            done = /*ImGui.*/ imgui_demo_1.ShowDemoWindow((value = show_demo_window) => show_demo_window = value);
-        }
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
-            // static float f = 0.0f;
-            // static int counter = 0;
-            ImGui.Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-            ImGui.Text("This is some useful text."); // Display some text (you can use a format strings too)
-            ImGui.Checkbox("Demo Window", (value = show_demo_window) => show_demo_window = value); // Edit bools storing our windows open/close state
-            ImGui.Checkbox("Another Window", (value = show_another_window) => show_another_window = value);
-            ImGui.SliderFloat("float", (value = f) => f = value, 0.0, 1.0); // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui.ColorEdit3("clear color", clear_color); // Edit 3 floats representing a color
-            if (ImGui.Button("Button")) // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui.SameLine();
-            ImGui.Text(`counter = ${counter}`);
+            ImGui.Begin("Annotator Supreme"); 
+            ImGui.Text("This is just a proof of concept... so it will be buggy");
             ImGui.Text(`Application average ${(1000.0 / ImGui.GetIO().Framerate).toFixed(3)} ms/frame (${ImGui.GetIO().Framerate.toFixed(1)} FPS)`);
-            ImGui.Checkbox("Memory Editor", (value = memory_editor.Open) => memory_editor.Open = value);
-            if (memory_editor.Open)
-                memory_editor.DrawWindow("Memory Editor", ImGui.bind.buffer);
-            const mi = ImGui.bind.mallinfo();
-            // ImGui.Text(`Total non-mmapped bytes (arena):       ${mi.arena}`);
-            // ImGui.Text(`# of free chunks (ordblks):            ${mi.ordblks}`);
-            // ImGui.Text(`# of free fastbin blocks (smblks):     ${mi.smblks}`);
-            // ImGui.Text(`# of mapped regions (hblks):           ${mi.hblks}`);
-            // ImGui.Text(`Bytes in mapped regions (hblkhd):      ${mi.hblkhd}`);
-            ImGui.Text(`Max. total allocated space (usmblks):  ${mi.usmblks}`);
-            // ImGui.Text(`Free bytes held in fastbins (fsmblks): ${mi.fsmblks}`);
-            ImGui.Text(`Total allocated space (uordblks):      ${mi.uordblks}`);
-            ImGui.Text(`Total free space (fordblks):           ${mi.fordblks}`);
-            // ImGui.Text(`Topmost releasable block (keepcost):   ${mi.keepcost}`);
 
             var screen_pos = ImGui.GetCursorScreenPos();
             ImGui.SetCursorScreenPos(ImGui.GetCursorScreenPos());
-
             if (screen_pos == screen_pos) {
                 if (image_gl_texture) {
                     const gl = ImGui_Impl.gl;
-
 
                     var x = ImGui.GetIO().MousePos.x-screen_pos.x;
                     var y = ImGui.GetIO().MousePos.y-screen_pos.y;
@@ -206,26 +164,10 @@ System.register(["imgui-js", "./imgui_impl", "imgui-js/imgui_demo", "imgui-js/im
                 ImGui.Text(image_url);
                 ImGui.EndTooltip();
             }
-            if (ImGui.Button("Sandbox Window")) {
-                show_sandbox_window = true;
-            }
-            if (show_sandbox_window)
-                ShowSandboxWindow("Sandbox Window", (value = show_sandbox_window) => show_sandbox_window = value);
-            ImGui.SameLine();
-            if (ImGui.Button("Gamepad Window")) {
-                show_gamepad_window = true;
-            }
-            if (show_gamepad_window)
-                ShowGamepadWindow("Gamepad Window", (value = show_gamepad_window) => show_gamepad_window = value);
-            ImGui.SameLine();
-            if (ImGui.Button("Movie Window")) {
-                show_movie_window = true;
-            }
-
+            
             if (ImGui.Button("Upload Images")) {
                 upload_images = !upload_images;
             }
-
             if (upload_images) {
                 const gl = ImGui_Impl.gl;
                 gl.canvas.style.left = "100px";
@@ -266,9 +208,7 @@ System.register(["imgui-js", "./imgui_impl", "imgui-js/imgui_demo", "imgui-js/im
                 const gl = ImGui_Impl.gl;
                 gl.canvas.style.left = "0px";
             }
-            if (show_movie_window) {
-                ShowMovieWindow("Movie Window", (value = show_movie_window) => show_movie_window = value);
-            }
+
             if (font) {
                 ImGui.PushFont(font);
                 ImGui.Text(`${font.GetDebugName()}`);
@@ -279,16 +219,6 @@ System.register(["imgui-js", "./imgui_impl", "imgui-js/imgui_demo", "imgui-js/im
             }
             ImGui.End();
         }
-        // 3. Show another simple window.
-        if (show_another_window) {
-            ImGui.Begin("Another Window", (value = show_another_window) => show_another_window = value, ImGui.WindowFlags.AlwaysAutoResize);
-            ImGui.Text("Hello from another window!");
-            if (ImGui.Button("Close Me"))
-                show_another_window = false;
-            ImGui.End();
-        }
-
-        
 
         ImGui.EndFrame();
         // Rendering
