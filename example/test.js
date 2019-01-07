83da86dc85ec69af2c0fdff42c88c9cc431cd043
Guilherme Fickel <guilhermefickel@gmail.com>
2019-01-07 21:00:10 -0200

    Adding test

---
+++ example/test.js
@@ -0,0 +1,13 @@
+(function () {
+
+    var Image = {};
+
+    Image.init = function () {
+        console.log('Image init');
+    }
+
+    Image.doSomething = function(ImGui) {
+        console.log('Doing something with ImGui');
+    }
+
+}());
